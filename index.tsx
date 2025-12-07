1129bf33fa9919a3eff91f94f2d270c9977be1ca
josefmayer15-code <josef.mayer15@gmail.com>
2025-12-07 11:23:12 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,14 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+// ***********************************************
+// 🛠️ KORREKTUR: Erzwingen der Root-Element-Existenz
+// ***********************************************
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  // Wenn 'root' nicht gefunden wird, Loggen wir einen Fehler, anstatt die App abstürzen zu lassen (gibt Debugging-Hinweis)
+  console.error("Could not find root element to mount to!"); 
+  // Wir erstellen es notfalls selbst, um die Anzeige zu erzwingen
+  const body = document.querySelector('body');
+  if (body) {
+    const newRoot = document.createElement('div');
+    newRoot.id = 'root';
+    body.appendChild(newRoot);
+    ReactDOM.createRoot(newRoot).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+} else {
+  // Standard-Initialisierung, wenn 'root' existiert
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
